e490efffcf4575def2e132f15dfd34b074c231ca
FOZLAUR <fozla@oocl.com>
2020-10-27 20:48:55 +0800

    [react-redux-todolist][Laurence&Erik] feature: Added constructor and made a design with label and x button

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -2,15 +2,25 @@ import React, { Component } from 'react';
 import { v4 as uuidv4 } from 'uuid'
 
 class ToDoItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id : ""
+        }
+    }
+
     setDone = () => {
-        //console.log(uuidv4());
+        console.log(uuidv4());
     }
 
     render() {
         const text = this.props.todoText;
         return (
             <div>
-                <label onClick={this.setDone}> hello there {text} </label>
+                <fieldset>
+                    <label onClick={this.setDone}> hello there {text} </label>
+                    <input type="button" value="x" onClick={this.setDone} />
+                </fieldset>
             </div>
         );
     }
