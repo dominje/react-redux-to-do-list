879a46320b9ec09df5cf29dcb6dc0cbaa222f3d0
FOZLAUR <fozla@oocl.com>
2020-10-27 22:21:38 +0800

    [react-redux-todolist][Laurence&Erik] feature: Added feature for update and delete item

--- src/actions.js
+++ src/actions.js
@@ -1,3 +1,11 @@
 export const addToDo = todo => {
     return {type: "ADD_ITEM", payload: todo};
 }
+
+export const updateToDo = id => {
+    return {type: "UPDATE_ITEM", payload: id};
+}
+
+export const deleteToDo = id => {
+    return {type: "DELETE_ITEM", payload: id};
+}
--- src/reducers.js
+++ src/reducers.js
@@ -4,6 +4,12 @@ const todo = (state = [], action) => {
     if (action.type === "ADD_ITEM"){
         return [...state,action.payload];
     }
+    if (action.type === "UPDATE_ITEM"){
+        return state.map(todo => todo.id === action.payload ? { ...todo, done: !todo.done } : todo);
+    }
+    if (action.type === "DELETE_ITEM"){
+        return state.filter(todo => todo.id !== action.payload);
+    }
     return state;
 }
 
