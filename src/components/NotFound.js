6945b679f33d562b3d8baa229afc61213cbf0b49
dominje <jericah.domingo@oocl.com>
2020-10-28 23:13:31 +0800

    [react-route-todolist][Laurence&Jericah] feature: added 404 message

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import logo from '../404.jpg';
 
 class NotFound extends Component {
 
     render() {
         return (
             <div>
-                <h1>ERROR 404 - PAGE NOT FOUND</h1>
+                <center>
+                    <h1>ERROR 404 - PAGE NOT FOUND</h1>
+                    <img src={logo}/>
+                </center>
             </div>
         );
     }
