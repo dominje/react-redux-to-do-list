d2e792595bd5c9a30111336569966299d3454bbc
FOZLAUR <fozla@oocl.com>
2020-10-28 19:27:22 +0800

    [react-route-todolist][Laurence&Jericah] refactor: Added NotFound.js as a 404 page and added some functionalities for Ant Design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 import ToDoList from './components/ToDoList';
+import NotFound from './components/NotFound';
 import DoneListContainer from './containers/DoneListContainer';
-import {HashRouter, BrowserRouter, Route, Link, Switch} from 'react-router-dom';
+import {HashRouter, BrowserRouter, Route, Link, Switch, NotFoundRoute} from 'react-router-dom';
+import React, { Component } from 'react';
+import {connect} from "react-redux"
+import {getTodos} from './apis/todos'
+import {initToDo} from './actions'
+import { Menu } from 'antd';
+import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends Component {
+  componentDidMount(){
+    getTodos().then(response =>{
+      console.log(response);
+      this.props.initToDo(response.data);
+    });
+  }
+
+  render() {
+    return (
+      <div className="App-header">
         <BrowserRouter>
           <Link to="/">Go Home, Young Prog</Link>
           <Link to="/donepage">Go Done, Young Prog</Link>
+          {/* <Menu defaultSelectedKeys={['home']} mode="horizontal">
+            <Menu.Item key="home" >
+              <Link to="/">Go Home, Young Prog</Link>
+            </Menu.Item>
+            <Menu.Item key="done" >
+            <Link to="/donepage">Go Done, Young Prog</Link>
+            </Menu.Item>
+          </Menu> */}
+          
           <Switch>
-            <Route path="/donepage" component={DoneListContainer}></Route>
-            <Route path="/" component={ToDoList}></Route>
+            <Route exact path="/donepage" component={DoneListContainer}></Route>
+            <Route exact path="/" component={ToDoList}></Route>
+            <Route path="*" component={NotFound}></Route>
           </Switch>
         </BrowserRouter>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  initToDo : (todos) => {dispatch(initToDo(todos))},
+})
+
+export default connect(null, mapDispatchToProps)(App);
