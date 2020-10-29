7a7b9a282b74f0a83f56bcb351c86d1c0daa88d2
dominje <jericah.domingo@oocl.com>
2020-10-29 20:44:45 +0800

    [Todo-List-React-Redux][Andrey&Jericah] Refactor: Integrate to Backend

--- src/apis/todos.js
+++ src/apis/todos.js
@@ -1,17 +1,17 @@
 import api from './api'
 
 export const getTodos = () => {
-    return api.get('api/todos')
+    return api.get('/todos')
 }
 
 export const addTodo = (text) => {
-    return api.post('api/todos', {text})
+    return api.post('/todos', {text})
 }
 
-export const updateTodo = (id,done) => {
-    return api.put(`api/todos/${id}`, {done: !done})
+export const updateTodo = (id,status) => {
+    return api.put(`/todos/${id}`, {status: !status})
 }
 
 export const deleteTodo = (id) => {
-    return api.delete(`api/todos/${id}`)
+    return api.delete(`/todos/${id}`)
 }
--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -5,9 +5,7 @@ import { Button } from 'antd';
 
 class ToDoItem extends Component {
 
-
-
-    setDone = event => {
+    updateItem = () => {
         updateTodo(this.props.todo.id,this.props.todo.done).then(response => {
             this.props.updateToDo(response.data.id);
         });
@@ -16,7 +14,6 @@ class ToDoItem extends Component {
 
     removeItem = () => {
         deleteTodo(this.props.todo.id).then(response =>{
-            console.log(response);
             this.props.deleteToDo(response.data.id);
         })
     };
@@ -29,10 +26,8 @@ class ToDoItem extends Component {
                 <fieldset>
                     <Button type="primary" size={'small'} onClick={this.removeItem} ghost>x</Button>
                     &nbsp;&nbsp;&nbsp;&nbsp;
-                    <label className={`${todoClass}`} onClick={this.setDone}> {text} </label>
-                  
+                    <label className={`${todoClass}`} onClick={this.updateItem}> {text} </label>
                 </fieldset>
-
         );
     }
 }
